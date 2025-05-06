ccab6338f354c057abeafa14a9bc35b02c27ff7f
komaltripathi05 <komaltripathi.kt05@gmail.com>
2025-05-06 13:59:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const inputs = document.querySelectorAll(".class-controller input");
+
+
+inputs.forEach((inputs) => inputs.addEventListener("change", update));
+
+function update() {
+    const suffixData = this.dataset.sizing || "";
+    document.documentElement.style.setProperty(
+        `--${this.name}`,
+        this.value + suffixData
+    );
+}
+
